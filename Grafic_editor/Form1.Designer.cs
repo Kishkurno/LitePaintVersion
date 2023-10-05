6183b12f08459307ba8955831575d0605c01b10f
Kishkurno <kishkurno.mari@mail.ru>
2023-10-05 23:47:06 +0300

    init

---
+++ Grafic_editor/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Grafic_editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1_Click";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.Location = new System.Drawing.Point(19, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(19, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(19, 203);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 41);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(19, 260);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(131, 41);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(19, 322);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(131, 41);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(19, 379);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(131, 41);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(186, 10);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(131, 41);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(186, 67);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(131, 41);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(60, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(269, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(335, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(453, 426);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Grafic_editor/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Grafic_editor
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics g;
+        Pen pen;
+        Bitmap bmp;
+
+        private Point old_coord;
+        private Point current_coord;
+        private string figure_type = "dot";
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pen = new Pen(Color.Black, 4);
+            g = Graphics.FromImage(bmp);
+        }
+
+        void DrawFigure()
+        {
+
+            int x = Math.Min(old_coord.X, current_coord.X);
+            int y = Math.Min(old_coord.Y, current_coord.Y);
+
+            switch (figure_type)
+            {
+
+                case "line":
+                    {
+                        g.DrawLine(pen, old_coord, current_coord);
+                        break;
+                    }
+                case "rectangle":
+                    {
+                        g.DrawRectangle(pen, x, y, Math.Abs(old_coord.X - current_coord.X), Math.Abs(old_coord.Y - current_coord.Y));
+                        break;
+                    }
+                case "sector":
+                    {
+
+                        g.DrawPie(pen, new Rectangle(x, y, Math.Abs(old_coord.X - current_coord.X), Math.Abs(old_coord.Y - current_coord.Y)), 90, 270);
+                        break;
+                    }
+                case "ellipse":
+                    {
+                        g.DrawEllipse(pen, x, y, Math.Abs(old_coord.X - current_coord.X), Math.Abs(old_coord.Y - current_coord.Y));
+                        break;
+                    }
+
+            }
+            pictureBox1.Image = bmp;
+
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                pen.Color = dialog.Color;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            figure_type = "rectangle";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            figure_type = "sector";
+        }
+
+        private void button6_Click (object sender, EventArgs e)
+        {
+            figure_type = "ellipse";
+        }
+
+        private void button7_Click (object sender, EventArgs e)
+        {
+            figure_type = "line";
+        }
+        private void button2_Click (object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+        }
+
+        private void pictureBox1_Click(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                old_coord = new Point(e.X, e.Y);
+            }
+
+        }
+
+        private void pictureBox1_Click_1 (object sender, MouseEventArgs e)
+        {
+            current_coord = new Point(e.X, e.Y);
+            DrawFigure();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            figure_type = "dot";
+        }
+        private void button8_Click (object sender, EventArgs e)
+        {
+            figure_type = "eraser";
+        }
+
+        private void pictureBox1_Click_2(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = e.X.ToString();
+            textBox2.Text = e.Y.ToString();
+            if (e.Button == MouseButtons.Left)
+            {
+                if (figure_type == "dot")
+                    g.DrawEllipse(new Pen(pen.Color, 3), e.X - 1, e.Y - 1, 2, 2);
+
+                else if (figure_type == "eraser")
+                {
+                    g.DrawEllipse(new Pen(Brushes.White, 3), e.X - 10, e.Y - 10, 20, 20);
+                    g.FillEllipse(Brushes.White, e.X - 10, e.Y - 10, 20, 20);
+                }
+
+            }
+            pictureBox1.Image = bmp;
+
+        }
+
+        private void button9_Click (object sender, EventArgs e)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.Filter = "PNG|*.png";
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(fileDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+            else
+            {
+                MessageBox.Show("Изображение не сохранено", "Диалог сохранения", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+     
+    }
+}
